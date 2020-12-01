70cf3f3ab61dddff8ec5a1b6a16165b99e84a336
NKrvavica <nino.krvavica@gmail.com>
2020-12-01 18:22:23 +0100

    removed list before itertools

--- Day01/day01.py
+++ Day01/day01.py
@@ -11,19 +11,20 @@ import itertools
 report = np.loadtxt(fname = 'input.txt', dtype ='int')
 
 # part one
-for a, b, in list(itertools.combinations(report, 2)):
+for a, b, in itertools.combinations(report, 2):
     if a + b == 2020:
         print(f'Solution to part one: {a * b}')
 
 # part two
-for a, b, c in list(itertools.combinations(report, 3)):
+for a, b, c in itertools.combinations(report, 3):
     if a + b + c == 2020:
         print(f'Solution to part two: {a * b * c}')
 
+# function that does the same as above but for any range of numbers
 def do_the_thing(list_of_numbers, sum_value, n):
-    for numbers in list(itertools.combinations(list_of_numbers, n)):
-        if np.array(numbers).sum() == sum_value:
-            return print(f'Solution: {np.array(numbers).prod()}')
-            
+    for numbers in itertools.combinations(list_of_numbers, n):
+        if sum(numbers) == sum_value:
+            return print(f'Solution: {np.prod(numbers)}')
+
 do_the_thing(report, 2020, 2)
 do_the_thing(report, 2020, 3)
