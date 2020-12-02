06dc05fa93053ed556ac80435cea9e45a7112b35
NKrvavica <nino.krvavica@gmail.com>
2020-12-02 21:09:26 +0100

    only parse input once

--- Day02/day02.py
+++ Day02/day02.py
@@ -8,7 +8,7 @@ Created on Wed Dec  2 08:21:21 2020
 fname = 'input.txt'
 
 with open(fname) as f:
-    lines = f.read().splitlines()
+    lines = f.readlines()
 
 
 def parse_input(line):
@@ -18,20 +18,21 @@ def parse_input(line):
     return low, high, letter, password
 
 
-# part 1
-counter = 0
-for line in lines:
-    low, high, letter, password = parse_input(line)
-    if low <= password.count(letter) <= high:
-        counter += 1
-
-print(f'First part. Number of valid passwords:{counter}')
-
-# part 2
-counter = 0
+counter_p1, counter_p2 = 0, 0
 for line in lines:
     first, second, letter, password = parse_input(line)
+    if first <= password.count(letter) <= second:
+        counter_p1 += 1
     if (password[first-1] == letter) ^ (password[second-1] == letter):
-        counter += 1
+        counter_p2 += 1
 
-print(f'Second part. Number of valid passwords:{counter}')
+print(f'First part. Number of valid passwords:{counter_p1}')
+print(f'Second part. Number of valid passwords:{counter_p2}')
+
+
+# NOTE!
+# alternative option for parsing input line
+# import re
+# words =re.compile(r'\w+')
+# low, high, letter, password = re.findall(words, line)
+# low, high = int(low), int(high)
