483a3d7cf214a07d892d5a61368fd7454336dd6f
NKrvavica <nino.krvavica@gmail.com>
2020-12-03 11:49:33 +0100

    changed the loop: move after, not before, you look for the tree

--- Day03/day03.py
+++ Day03/day03.py
@@ -6,34 +6,32 @@ Created on Thu Dec  3 08:08:50 2020
 """
 
 
-with open('input.txt', 'r') as f:
-    mapp = f.read().split('\n')
-
-
 def find_trees(mapp, step):
-    nr_cols = len(mapp[0])
-    x, y = [0, 0]
+    x, y = step
     tree_counter = 0
-    while y < len(mapp)-1:
-        y += step[1] # move down
-        x = (x + step[0]) % nr_cols # move right
+    while y < map_height:
         if mapp[y][x] == '#':
             tree_counter += 1
+        y += step[1] # move down
+        x = (x + step[0]) % map_width # move right
     return tree_counter
 
 
+# read the input
+with open('input.txt', 'r') as f:
+    mapp = f.read().split('\n')
+
+# get the map size    
+map_height, map_width = len(mapp), len(mapp[0])
+
 # part 1
 step = (3, 1)
-how_many_trees = find_trees(mapp, step)
-print(f'Solution to part 1. Trees encountered: {how_many_trees}')
-
+trees_in_part_one = find_trees(mapp, step)
+print(f'Solution to part 1. Trees encountered: {trees_in_part_one}')
 
 # part 2
-steps = [(1, 1),
-         (5, 1),
-         (7, 1),
-         (1, 2)]
-tree_multiplyer = how_many_trees # start with the solution to part 1
+steps = [(1, 1), (5, 1), (7, 1), (1, 2)]
+tree_multiplyer = trees_in_part_one # start with the solution to part 1
 for step in steps:
     how_many_trees = find_trees(mapp, step)
     tree_multiplyer *= how_many_trees
