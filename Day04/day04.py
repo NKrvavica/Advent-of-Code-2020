6ea84d33672e375282dace6491e3494c037877b8
NKrvavica <nino.krvavica@gmail.com>
2020-12-04 11:15:07 +0100

    day 04: trying out pandas

---
+++ Day04/day04.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 08:21:58 2020
+
+@author: Nino
+"""
+
+import pandas as pd
+import re
+
+
+def parse_input(lines):
+    passports = pd.DataFrame()
+    new_passport = {}
+    for line in lines:
+        data = line.split(' ')
+        if data == ['']: # new passport
+            passports = passports.append(new_passport, ignore_index=True)
+            new_passport = {}
+        else:
+            for d in data:
+                field, value = d.split(':')
+                new_passport[field] = value
+    return passports
+
+
+
+def is_hex(string):
+    if re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', string):
+        return True
+    return False    
+
+
+
+with open('input.txt') as f:
+    lines = f.read().split('\n')
+
+passports = parse_input(lines)
+
+# part 1
+passport_without_cid = passports.drop(['cid'], axis=1) # ignore 'cid'
+passports['valid'] = passport_without_cid.isnull().sum(axis=1) == False
+valid_pass = passports[passports['valid'] == True]
+print(f'Solution to part 1. Valid passports: {len(valid_pass)}')
+
+# part 2
+valid_pass = valid_pass.convert_dtypes()
+
+valid_pass['byr'] = pd.to_numeric(valid_pass['byr'], errors='coerce')
+valid_pass = valid_pass[valid_pass['byr'].between(1920, 2002)]
+
+valid_pass['iyr'] = pd.to_numeric(valid_pass['iyr'], errors='coerce')
+valid_pass = valid_pass[valid_pass['iyr'].between(2010, 2020)]
+
+valid_pass['eyr'] = pd.to_numeric(valid_pass['eyr'], errors='coerce')
+valid_pass = valid_pass[valid_pass['eyr'].between(2020, 2030)]
+
+valid_pass = valid_pass[valid_pass['hgt'].str.contains('cm|in', regex=True)]
+valid_pass[['hgt_unit']] = valid_pass['hgt'].str[-2:]
+valid_pass[['hgt_value']] = valid_pass['hgt'].str[:-2]
+valid_pass['hgt_value'] = pd.to_numeric(valid_pass['hgt_value'], errors='coerce')
+valid_cm_height = ((valid_pass['hgt_value'].between(150, 193)) & (valid_pass['hgt_unit']=='cm'))
+valid_in_height = ((valid_pass['hgt_value'].between(59, 76)) & (valid_pass['hgt_unit']=='in'))
+valid_pass = valid_pass[valid_cm_height | valid_in_height]
+
+valid_pass = valid_pass[[is_hex(hcl) for hcl in valid_pass['hcl']]]
+
+valid_eye_colors = set(['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])
+valid_pass = valid_pass[valid_pass['ecl'].isin(valid_eye_colors)]
+
+valid_pass = valid_pass[valid_pass['pid'].str.len()==9]
+
+print(f'Solution to part 2. Valid passports: {len(valid_pass)}')
+
