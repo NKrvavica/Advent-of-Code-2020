663fe08cab2214d62be15b77ae03d16b7489587c
NKrvavica <nino.krvavica@gmail.com>
2020-12-04 22:44:02 +0100

    better input parsing using \n\n and some minor code cleanup

--- Day04/day04.py
+++ Day04/day04.py
@@ -11,36 +11,31 @@ import re
 
 def parse_input(lines):
     passports = pd.DataFrame()
-    new_passport = {}
     for line in lines:
-        data = line.split(' ')
-        if data == ['']: # new passport
-            passports = passports.append(new_passport, ignore_index=True)
-            new_passport = {}
-        else:
-            for d in data:
-                field, value = d.split(':')
-                new_passport[field] = value
+        line = line.replace('\n', ' ').split(' ')
+        new_passport = {}
+        for l in line:
+            field, value = l.split(':')
+            new_passport[field] = value
+        passports = passports.append(new_passport, ignore_index=True)
     return passports
 
 
-
 def is_hex(string):
-    if re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', string):
+    if re.search(r'^#(?:[0-9a-f]{3}){1,2}$', string):
         return True
     return False    
 
 
 
 with open('input.txt') as f:
-    lines = f.read().split('\n')
+    lines = f.read().split('\n\n')
 
 passports = parse_input(lines)
 
 # part 1
-passport_without_cid = passports.drop(['cid'], axis=1) # ignore 'cid'
-passports['valid'] = passport_without_cid.isnull().sum(axis=1) == False
-valid_pass = passports[passports['valid']]
+passports.drop(['cid'], axis=1, inplace=True) # ignore 'cid'
+valid_pass = passports[passports.isnull().sum(axis=1) == False]
 print(f'Solution to part 1. Valid passports: {len(valid_pass)}')
 
 # part 2
@@ -49,17 +44,17 @@ num_columns = ['byr', 'iyr', 'eyr']
 valid_pass[num_columns] = valid_pass[num_columns].apply(pd.to_numeric)
 
 valid_pass = valid_pass[valid_pass['byr'].between(1920, 2002)]
-
 valid_pass = valid_pass[valid_pass['iyr'].between(2010, 2020)]
-
 valid_pass = valid_pass[valid_pass['eyr'].between(2020, 2030)]
 
 valid_pass = valid_pass[valid_pass['hgt'].str.contains('cm|in', regex=True)]
 valid_pass[['hgt_unit']] = valid_pass['hgt'].str[-2:]
 valid_pass[['hgt_value']] = valid_pass['hgt'].str[:-2]
 valid_pass['hgt_value'] = pd.to_numeric(valid_pass['hgt_value'])
-valid_cm_height = ((valid_pass['hgt_value'].between(150, 193)) & (valid_pass['hgt_unit']=='cm'))
-valid_in_height = ((valid_pass['hgt_value'].between(59, 76)) & (valid_pass['hgt_unit']=='in'))
+valid_cm_height = ((valid_pass['hgt_value'].between(150, 193))
+                   & (valid_pass['hgt_unit']=='cm'))
+valid_in_height = ((valid_pass['hgt_value'].between(59, 76))
+                   & (valid_pass['hgt_unit']=='in'))
 valid_pass = valid_pass[valid_cm_height | valid_in_height]
 
 valid_pass = valid_pass[[is_hex(hcl) for hcl in valid_pass['hcl']]]
