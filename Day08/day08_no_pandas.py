a2371db0f0d21feef083cf8b14dc5405cdc7cb1e
NKrvavica <nino.krvavica@gmail.com>
2020-12-08 16:50:04 +0100

    added a solution without pandas, much faster!

--- Day08/day08_no_pandas.py
+++ Day08/day08_no_pandas.py
@@ -8,22 +8,24 @@ Created on Tue Dec  8 09:19:05 2020
 import pandas as pd
 
 # load data
-program = pd.read_csv('input.txt', header=None, sep=' ',
-                      names=['op', 'arg'])
+with open('./input.txt', 'r') as f:
+    program = f.read().split('\n')
+    
 program_len = len(program)
 
 # run the program
 def run_program(program, program_len):
     idx, accumulator = 0, 0
-    program['visited']= False
+    visited = set()
 
     while True:  
-        op, arg, visited = program.loc[idx]
+        op, arg = program[idx].split(' ')
+        arg = int(arg)
 
-        if visited:
-            return program, accumulator, idx
+        if idx in visited:
+            return accumulator, idx
         else:
-            program.loc[idx, 'visited'] = True
+            visited.add(idx)
 
         if op == 'nop':
             idx += 1 
@@ -36,32 +38,31 @@ def run_program(program, program_len):
         if idx >= program_len: # this is needed for part 2
             print('Warning! Attempting to run the instruction below the last',
                   'instruction in the file')
-            return program, accumulator, idx
+            return accumulator, idx
     
     
 # part 1
-program, accumulator, idx = run_program(program, program_len)
+accumulator, idx = run_program(program, program_len)
 print(f'Solution to part 1: {accumulator} \n')
 
 
 # part 2
 def attempt_brute_forces_changes(program):
     program_len = len(program)
-    for i, row in program.iterrows():
-        op, arg, visited = row
+    for i, row in enumerate(program):
 
-        if op == 'nop':
+        if row[:3] == 'nop':
             program_copy = program.copy()
-            program_copy.loc[i, 'op'] = 'jmp'
-            print(f'attempt changing line {i}, from nop to jmp')
-        elif op == 'jmp':
+            program_copy[i] = 'jmp' + row[3:]
+            # print(f'attempt changing line {i}, from nop to jmp')
+        elif row[:3] == 'jmp':
             program_copy = program.copy()
-            program_copy.loc[i, 'op'] = 'nop'
-            print(f'attempt changing line {i}, from jmp to nop,')
+            program_copy[i] = 'nop' + row[3:]
+            # print(f'attempt changing line {i}, from jmp to nop,')
         else:
             continue
 
-        program_copy, accumulator, idx = run_program(program_copy, program_len)
+        accumulator, idx = run_program(program_copy, program_len)
         # print(f'program terminated at line {idx}')
 
         if idx >= program_len:
