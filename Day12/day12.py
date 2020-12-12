caf08d172c6a725e0a7aff3cbf2e877fec2e5c51
NKrvavica <nino.krvavica@gmail.com>
2020-12-12 22:38:39 +0100

    single navigate function for part 1&2, and more efficient turn func

--- Day12/day12.py
+++ Day12/day12.py
@@ -5,8 +5,9 @@ Created on Sat Dec 12 08:29:15 2020
 @author: Nino
 """
 
-import math
-import numpy as np
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
 
 def parse_input(fname):
     with open(fname, 'r') as f:
@@ -17,38 +18,10 @@ def parse_input(fname):
     return instructions
 
 
-# =============================================================================
-# PART 1 FUNCTIONS
-# =============================================================================
-
-
 def move(x, y, inst, value):
-    x += MOVES[inst][0] * value
-    y += MOVES[inst][1] * value
+    x += DIRS[inst][0] * value
+    y += DIRS[inst][1] * value
     return x, y
-
-
-def navigate(x, y, dir_idx, instructions):
-    for inst, value in instructions:
-        if inst in DIRS:
-            x, y = move(x, y, inst, value)
-        elif inst in TURNS:
-            dir_idx = (dir_idx + TURNS[inst] * value // 90) % 4
-        elif inst == 'F':
-            direction = DIRS[dir_idx]
-            x, y = move(x, y, direction, value)
-    return abs(x) + abs(y)
-
-
-# =============================================================================
-# PART 2 FUNCTIONS
-# =============================================================================
-
-
-def move_waypoint(xway, yway, inst, value):
-    xway += MOVES[inst][0] * value
-    yway += MOVES[inst][1] * value
-    return xway, yway
 
 
 def move_ship(x, y, xway, yway, value):
@@ -57,32 +30,24 @@ def move_ship(x, y, xway, yway, value):
     return x, y
 
 
-def cart2pol(i, j):
-    rho = math.sqrt(i**2 + j**2)
-    phi = math.atan2(j, i)
-    return(rho, phi)
+def turn(xway, yway, inst, value):
+    sign = TURNS[inst]
+    for _ in range(value // 90):
+        xway, yway = sign * yway, -sign * xway
+    return xway, yway
 
 
-def pol2cart(rho, phi):
-    i = int(round(rho * math.cos(phi)))
-    j = int(round(rho * math.sin(phi)))
-    return(i, j)
-
-
-def rotate(xway, yway, inst, value):
-    rho, phi = cart2pol(xway, yway)
-    phi += -TURNS[inst] * math.radians(value)
-    return pol2cart(rho, phi)
-
-
-def navigate2(x, y, xway, yway, instructions):
+def navigate(x, y, xway, yway, dir_idx, instructions, part=1):
     for inst, value in instructions:
         if inst in DIRS:
-            xway, yway = move_waypoint(xway, yway, inst, value)
+            if part==1:     x, y = move(x, y, inst, value)
+            else:           xway, yway = move(xway, yway, inst, value)
         elif inst in TURNS:
-            xway, yway = rotate(xway, yway, inst, value)
+            if part==1:     dir_idx = (dir_idx + TURNS[inst] * value // 90) % 4
+            else:           xway, yway = turn(xway, yway, inst, value)
         elif inst == 'F':
-            x, y = move_ship(x, y, xway, yway, value)
+            if part==1:     x, y = move(x, y, [*DIRS][dir_idx], value)
+            else:           x, y = move_ship(x, y, xway, yway, value)
     return (abs(x) + abs(y))
 
 
@@ -93,21 +58,19 @@ def navigate2(x, y, xway, yway, instructions):
 # load input
 instructions = parse_input('input.txt')
 
-#definitions
-DIRS = ['E', 'S', 'W', 'N']
-MOVES = {'E':(1, 0), 'S':(0, -1), 'W':(-1, 0), 'N':(0, 1)}
-TURNS = {'R':1, 'L':-1}
-ROT_MAT = np.array([[-1, 0], [0, 1]])
+# definitions
+DIRS = {'E':(1, 0), 'S':(0, -1), 'W':(-1, 0), 'N':(0, 1)}
+TURNS = {'R': 1, 'L': -1}
 
 # part 1
 x, y = 0, 0
 dir_idx = 0
-p1  = navigate(x, y, dir_idx, instructions)
+p1  = navigate(x, y, None, None, dir_idx, instructions, part=1)
 
 # part 2
 x, y = 0, 0
 xway, yway = 10, 1
-p2 = navigate2(x, y, xway, yway, instructions)
+p2 = navigate(x, y, xway, yway, None, instructions, part=2)
 
 
 print(f'Solution to part 1: {p1}')
