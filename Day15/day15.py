bea4e236d5663ee3160153536fb4e62c6edf5298
NKrvavica <nino.krvavica@gmail.com>
2020-12-15 18:57:28 +0100

    single function and further improvement with d.get()

--- Day15/day15.py
+++ Day15/day15.py
@@ -8,36 +8,27 @@ Created on Tue Dec 15 08:40:39 2020
 from time import time
 
 
-def speak_given_numbers(given_numbers):
+def play_the_game(given_numbers, end):
     spoken_numbers = {}
     for i, nr in enumerate(given_numbers):
         spoken_numbers[nr] = i+1
-    return spoken_numbers, given_numbers[-1]
-
-
-def play_the_game(start, spoken_numbers, last_spoken, end):
-    for turn in range(start, end):
-        if last_spoken in spoken_numbers:
-            speak = turn - spoken_numbers[last_spoken]
-        else: speak = 0
+    last_spoken = given_numbers[-1]
+    for turn in range(len(given_numbers), end):
+        speak = turn - spoken_numbers.get(last_spoken, turn)
         spoken_numbers[last_spoken] = turn
-        # print(f'{turn}:, last spoken: {last_spoken}, speak: {speak}')
         last_spoken = speak
-    return spoken_numbers, last_spoken
+    return last_spoken
 
 
 msStart = time()
 
 given_numbers = [8,0,17,4,1,12]
 
-spoken_numbers, last_spoken = speak_given_numbers(given_numbers)
+last_spoken1 = play_the_game(given_numbers, 2020)
+last_spoken2 = play_the_game(given_numbers, 30000000)
 
-spoken_numbers, last_spoken1 = play_the_game(6, spoken_numbers,
-                                             last_spoken, 2020)
-
-spoken_numbers, last_spoken2 = play_the_game(2020, spoken_numbers,
-                                             last_spoken1, 30000000)
 
 print(f'Solution to part 1: {last_spoken1}\n')
 print(f'Solution to part 2: {last_spoken2}\n')
+
 print(f'Run time: {time() - msStart:.2f} s')
