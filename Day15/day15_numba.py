bec21602032737d1f079019c1e475806d7b5f352
NKrvavica <nino.krvavica@gmail.com>
2020-12-15 14:30:39 +0100

    simplified to a single function

--- Day15/day15_numba.py
+++ Day15/day15_numba.py
@@ -11,35 +11,25 @@ import numpy as np
 
 
 @njit
-def speak_given_numbers(given_numbers):
+def play_the_game(given_numbers, end):
+    last_spoken = given_numbers[-1]
     spoken_numbers = {}
     for i, nr in enumerate(given_numbers):
         spoken_numbers[nr] = i+1
-    return spoken_numbers, given_numbers[-1]
-
-
-@njit
-def play_the_game(start, spoken_numbers, last_spoken, end):
-    for turn in range(start, end):
+    for turn in range(len(given_numbers), end):
         if last_spoken in spoken_numbers:
             speak = turn - spoken_numbers[last_spoken]
         else: speak = 0
         spoken_numbers[last_spoken] = turn
         last_spoken = speak
-    return spoken_numbers, last_spoken
+    return last_spoken
 
 
 msStart = time()
 
 given_numbers = np.array([8,0,17,4,1,12])
-
-spoken_numbers, last_spoken = speak_given_numbers(given_numbers)
-
-spoken_numbers, last_spoken1 = play_the_game(6, spoken_numbers,
-                                             last_spoken, 2020)
-
-spoken_numbers, last_spoken2 = play_the_game(2020, spoken_numbers,
-                                             last_spoken1, 30000000)
+last_spoken1 = play_the_game(given_numbers, 2020)
+last_spoken2 = play_the_game(given_numbers, 30000000)
 
 print(f'Solution to part 1: {last_spoken1}\n')
 print(f'Solution to part 2: {last_spoken2}\n')
