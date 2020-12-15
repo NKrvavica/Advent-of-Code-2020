c07a301fa3630397944328b7d0fe96380e9c9839
NKrvavica <nino.krvavica@gmail.com>
2020-12-15 10:03:59 +0100

    day15: pure python and number version (speedup ~3x)

---
+++ Day15/day15.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 15 08:40:39 2020
+
+@author: Nino
+"""
+
+from time import time
+
+
+def speak_given_numbers(given_numbers):
+    spoken_numbers = {}
+    for i, nr in enumerate(given_numbers):
+        spoken_numbers[nr] = i+1
+    return spoken_numbers, given_numbers[-1]
+
+
+def play_the_game(start, spoken_numbers, last_spoken, end):
+    for turn in range(start, end):
+        if last_spoken in spoken_numbers:
+            speak = turn - spoken_numbers[last_spoken]
+        else: speak = 0
+        spoken_numbers[last_spoken] = turn
+        # print(f'{turn}:, last spoken: {last_spoken}, speak: {speak}')
+        last_spoken = speak
+    return spoken_numbers, last_spoken
+
+
+msStart = time()
+
+given_numbers = [8,0,17,4,1,12]
+
+spoken_numbers, last_spoken = speak_given_numbers(given_numbers)
+
+spoken_numbers, last_spoken1 = play_the_game(6, spoken_numbers,
+                                             last_spoken, 2020)
+
+spoken_numbers, last_spoken2 = play_the_game(2020, spoken_numbers,
+                                             last_spoken1, 30000000)
+
+print(f'Solution to part 1: {last_spoken1}\n')
+print(f'Solution to part 2: {last_spoken2}\n')
+print(f'Run time: {time() - msStart:.2f} s')
