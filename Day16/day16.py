150e029472c4c1bdacbcc7e5643a4c164ec0f263
NKrvavica <nino.krvavica@gmail.com>
2020-12-16 13:20:20 +0100

    day 16, seperated the code into several functions

--- Day16/day16.py
+++ Day16/day16.py
@@ -8,6 +8,12 @@ Created on Wed Dec 16 07:32:33 2020
 import re
 import numpy as np
 from collections import defaultdict
+from time import time
+
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
 
 def load_input(fname):
     with open(fname, 'r') as f:
@@ -38,43 +44,57 @@ def parse_input(fields, my_ticket, nearby_tickets):
     return valid_fields, my_ticket_numbers, ticket_numbers
 
 
+def find_valid_tickets(ticket_fields, nearby_tickets):
+    all_valid_field_values = set.union(*ticket_fields.values())
+    valid_tickets = []
+    p1 = 0
+    for ticket in nearby_tickets:
+        diff = list(set(ticket).difference(all_valid_field_values))
+        if diff: p1 += sum(diff)
+        else: valid_tickets.append(ticket)
+    return valid_tickets, p1
+
+
+
+def connect_values_with_ticket_fields(ticket_fields, valid_tickets):
+     # numpy, yay! I can just transpose the matrix
+    valid_tickets = np.array(valid_tickets)
+
+    # find which number indices are valid for each field
+    valid_idx = defaultdict(set)
+    for nr, place in enumerate(valid_tickets.T):
+        for key, valid_nr in ticket_fields.items():
+            if set(place).issubset(valid_nr):
+                valid_idx[key].add(nr)
+    
+    # find which field value corresponds to which row
+    sorted_places = sorted(valid_idx.items(), key=lambda x: x[1])
+    not_it = set()
+    for key, value in sorted_places:
+        valid_idx[key] = valid_idx[key].difference(not_it)
+        not_it.update(value)
+    
+    return valid_idx
+
+
+def define_my_ticket(valid_idx):
+    p2 = 1   
+    for key, idx in valid_idx.items():
+        if key[:6] == 'depart':
+            p2 *= my_ticket[list(idx)[0]]
+    return p2
+
+
+# =============================================================================
+# MAIN
+# =============================================================================
+msStart = time()
 data = load_input('input.txt')
-fields, my_ticket, nearby_tickets = parse_input(*data)
+ticket_fields, my_ticket, nearby_tickets = parse_input(*data)
+valid_tickets, invalid_sum = find_valid_tickets(ticket_fields, nearby_tickets)
+valid_idx = connect_values_with_ticket_fields(ticket_fields, valid_tickets)
+p2 = define_my_ticket(valid_idx)
 
-
-# part 1
-all_valid_field_values = set.union(*fields.values())
-valid_tickets = []
-p1 = 0
-for ticket in nearby_tickets:
-    diff = list(set(ticket).difference(all_valid_field_values))
-    if diff: p1 += sum(diff)
-    else: valid_tickets.append(ticket)
-
-
-# part 2
-valid_tickets = np.array(valid_tickets) # numpy, yay! I can just transpose the matrix
-
-# find which number indices are valid for each field
-valid_idx = defaultdict(set)
-for nr, place in enumerate(valid_tickets.T):
-    for key, valid_nr in fields.items():
-        if set(place).issubset(valid_nr):
-            valid_idx[key].add(nr)
-
-# find which field value corresponds to which row
-sorted_places = sorted(valid_idx.items(), key=lambda x: x[1])
-not_it = set()
-for key, value in sorted_places:
-    valid_idx[key] = valid_idx[key].difference(not_it)
-    not_it.update(value)
-
-# finally! get numbers in departure fields from my ticket and multiply them
-p2 = 1   
-for key, idx in valid_idx.items():
-    if key[:6] == 'depart':
-        p2 *= my_ticket[list(idx)[0]]
-
-
-print(f'Solution to part 1: {p1}')
+print(f'Solution to part 1: {invalid_sum}')
 print(f'Solution to part 2: {p2}')
+print(f'Run time: {time() - msStart:.3f} s')
