6a597a14622aa46414abc60a15cb7124849783ea
Nino Krvavica <35114994+NKrvavica@users.noreply.github.com>
2020-12-25 20:15:06 +0100

    numba for faster run times

--- Day24/day24.py
+++ Day24/day24.py
@@ -4,9 +4,12 @@ Created on Thu Dec 24 09:38:10 2020
 
 @author: Nino
 """
-
+import math
+import matplotlib.pyplot as plt
 import numpy as np
 from time import time
+from numba import njit
+from numba import boolean
 
 msStart = time()
 
@@ -18,7 +21,6 @@ def load_input(fname):
 
 def parse_input(instructions):
     parsed_instruc = []
-    max_len = 0
     for instruction in instructions:
         tiles = list(instruction)
         instruc = []
@@ -28,12 +30,11 @@ def parse_input(instructions):
             else:
                 instruc.append(tiles.pop(0))
         parsed_instruc.append(instruc)
-        if len(instruc) > max_len: max_len = len(instruc)
-    return parsed_instruc, max_len
+    return parsed_instruc
 
 
-def turn_tiles(parsed_instruc):
-    floor = np.zeros((max_len*8, max_len*8), dtype='bool')    
+def turn_tiles(parsed_instruc, max_len):
+    floor = np.zeros((max_len, max_len), dtype='bool')    
     for instructions in parsed_instruc:
         # print(instruc)
         x, y = floor.shape[0]//2, floor.shape[1]//2
@@ -52,61 +53,63 @@ MOVE = {'e': (1, 0),
 
 
 instructions = load_input('input.txt')
-parsed_instruc, max_len = parse_input(instructions)
+parsed_instruc = parse_input(instructions)
 
 # part 1
-floor = turn_tiles(parsed_instruc)
+max_len = 150
+floor = turn_tiles(parsed_instruc, max_len)
 print(f'Solution to part 1: {floor.sum()}')
 
 
-def living_art_exhibit(floor, days):
-    for day in range(100):
-        
+# boolean = 'bool'
+@njit
+def living_art_exhibit(floor, days, max_len):
+    for day in range(days):        
         # new day, new floor
-        new_floor = np.zeros((max_len*8, max_len*8), dtype='bool')    
-        
+        new_floor = np.zeros(shape=(max_len, max_len),
+                             dtype=boolean)    
         # boundaries to analyse
-        blacks = np.argwhere(floor)
-        down = blacks[:, 0].min() - 1
-        up = blacks[:, 0].max() + 1
-        left = blacks[:, 1].min() - 1
-        right = blacks[:, 1].max() + 1
-        
+        vertical_boundary = np.argwhere(floor)
+        down = vertical_boundary[:, 0].min() - 1
+        up = vertical_boundary[:, 0].max() + 1        
         for i, row in enumerate(floor[down:up+1], down):
+            horizontal_boundary = np.argwhere(floor[i-1:i+2])
+            left = horizontal_boundary[:, 1].min() - 1
+            right = horizontal_boundary[:, 1].max() + 1
             for j, tile in enumerate(row[left:right+1], left):
                 black_tiles = (floor[i-1:i+2, j-1:j+2].sum()
                                - np.diag(floor[i-1:i+2, j-1:j+2]).sum())
                 if tile and 0 < black_tiles <= 2:
                     new_floor[i, j] = True
                 elif not tile and black_tiles == 2:
-                    new_floor[i, j] = True
-    
+                    new_floor[i, j] = True    
         floor = new_floor
     return floor
 
 
 # part 2
-floor = living_art_exhibit(floor, days=100)
+floor = living_art_exhibit(floor, days=100, max_len=max_len)
 print(f'Solution to part 2: {floor.sum()}')
-
 print(f'Run time: {time() - msStart:.6f} s')
 
 
-# plot
-blacks = np.argwhere(floor)
-down = blacks[:, 0].min() - 1
-up = blacks[:, 0].max() + 1
-left = blacks[:, 1].min() - 1
-right = blacks[:, 1].max() + 1
-
-import math
+# =============================================================================
+# PLOT
+# =============================================================================
 def pointy_hex_to_pixel(q, r, size=1):
     x = size * (math.sqrt(3) * q  +  math.sqrt(3)/2 * r)
     y = size * (3/2 * r)
     return np.array([x, y, 1])
 
+vertical_boundary = np.argwhere(floor)
+down = vertical_boundary[:, 0].min() - 1
+up = vertical_boundary[:, 0].max() + 1
+
 black_tiles, white_tiles = [], []
 for i, row in enumerate(floor[down:up+1], down):
+    horizontal_boundary = np.argwhere(floor[i-1:i+2])
+    left = horizontal_boundary[:, 1].min() - 1
+    right = horizontal_boundary[:, 1].max() + 1
     for j, tile in enumerate(row[left:right+1], left):
         if tile:
             black_tiles.append(pointy_hex_to_pixel(i, j))
@@ -116,9 +119,8 @@ for i, row in enumerate(floor[down:up+1], down):
 black_tiles = np.array(black_tiles)
 white_tiles = np.array(white_tiles)
 
-import matplotlib.pyplot as plt
-
-plt.figure()
+fig, ax = plt.subplots()
 plt.scatter(white_tiles[:, 0], white_tiles[:, 1], s=30, c='w', edgecolors='k',
             marker='h', linewidth=0.1)
 plt.scatter(black_tiles[:, 0], black_tiles[:, 1], s=30, c='k', marker='h')
+ax.set_aspect('equal')
